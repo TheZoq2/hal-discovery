8b8c301a0c738a8930d5df53ca1b647ec0cf7847
Christian Meusel <christian.meusel@posteo.de>
2020-07-19 19:56:46 +0200

    WIP on first lines of f3discovery example

--- f3discovery/src/main.rs
+++ f3discovery/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+
+use cortex_m_rt::entry;
+use panic_semihosting as _;
+use stm32f3xx_hal as hal;
+
+use hal::stm32;
+
+
+#[entry]
+fn main() -> ! {
+    // TODO: How to get the peripherals set up correctly?
+    // let mut cp = cortex_m::Peripherals::take().unwrap();
+    let dp = stm32::Peripherals::take().unwrap();
+
+    // let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
+    // let mut led = gpioe.pe3.into_push_pull_output(
+    //     &mut gpioe.moder, &mut gpioe.otyper);
+
+    // led.set_high().unwrap();
+
+    loop {
+        // Don't let this loop get optimized away.
+        continue;
+    }
 }
