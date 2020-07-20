bb19a8a281ffc1cff928d68c1a812d029cee5397
Christian Meusel <christian.meusel@posteo.de>
2020-07-20 09:42:23 +0200

    Turn on LD3 (north) in f3discovery example

--- f3discovery/src/main.rs
+++ f3discovery/src/main.rs
@@ -3,23 +3,25 @@
 
 
 use cortex_m_rt::entry;
+use embedded_hal::digital::v2::OutputPin;
 use panic_semihosting as _;
 use stm32f3xx_hal as hal;
 
 use hal::stm32;
+use hal::prelude::*;
+
 
 
 #[entry]
 fn main() -> ! {
-    // TODO: How to get the peripherals set up correctly?
-    // let mut cp = cortex_m::Peripherals::take().unwrap();
     let dp = stm32::Peripherals::take().unwrap();
+    let mut rcc = dp.RCC.constrain();
+    let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
 
-    // let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
-    // let mut led = gpioe.pe3.into_push_pull_output(
-    //     &mut gpioe.moder, &mut gpioe.otyper);
+    let mut led_pin = gpioe.pe9.into_push_pull_output(
+        &mut gpioe.moder, &mut gpioe.otyper);
 
-    // led.set_high().unwrap();
+    led_pin.set_high().unwrap();
 
     loop {
         // Don't let this loop get optimized away.
